d1a8ae5149947cd7d7376814aad685bf43a4f732
Jessica Jordan <jshenelle@me.com>
2020-01-27 00:01:12 -0500

    added two functions

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,7 @@ function sent(){
     alert("Thank you for reaching out.");
     console.log('message sent to database')
 }
+
+function ref(){
+    alert("All references will be provided upon serious inquiries only.");
+}
